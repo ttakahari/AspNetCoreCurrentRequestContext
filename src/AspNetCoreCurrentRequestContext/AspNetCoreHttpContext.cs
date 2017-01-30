8521f7eb47b3841178e7abc5b81ec6b4b5be8b2f
t.takahari <taiga1018@gmail.com>
2017-01-31 01:06:00 +0900

    add sources.

---
+++ src/AspNetCoreCurrentRequestContext/AspNetCoreHttpContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Threading;
+
+namespace AspNetCoreCurrentRequestContext
+{
+    /// <summary>
+    /// The class that manages <see cref="HttpContext"/> in the current request.
+    /// </summary>
+    public class AspNetCoreHttpContext
+    {
+        private static readonly AsyncLocal<HttpContext> _context = new AsyncLocal<HttpContext>();
+
+        /// <summary>
+        /// Get <see cref="HttpContext"/> in this request.
+        /// </summary>
+        public static HttpContext Current
+        {
+            get { return _context.Value; }
+            internal set { _context.Value = value; }
+        }
+    }
+}
---
+++ src/AspNetCoreCurrentRequestContext/CurrentRequestContextMiddleware.cs
@@ -0,0 +1,62 @@
+﻿using AspNetCoreCurrentRequestContext;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Threading.Tasks;
+
+namespace AspNetCoreCurrentRequestContext
+{
+    /// <summary>
+    /// Enable using <see cref="AspNetCoreHttpContext.Current"/>. 
+    /// </summary>
+    public class CurrentRequestContextMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        /// <summary>
+        /// Create a new instance of <see cref="CurrentRequestContextMiddleware"/>.
+        /// </summary>
+        /// <param name="next">The next middleware in the pipeline.</param>
+        public CurrentRequestContextMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        /// <summary>
+        /// Execute the application's request pipeline.
+        /// </summary>
+        /// <param name="context"><see cref="HttpContext"/> in this request.</param>
+        /// <returns>The task.</returns>
+        public async Task Invoke(HttpContext context)
+        {
+            AspNetCoreHttpContext.Current = context;
+
+            await _next(context);
+
+            AspNetCoreHttpContext.Current = null;
+        }
+    }
+}
+
+namespace Microsoft.AspNetCore.Builder
+{
+    /// <summary>
+    /// Extensions of <see cref="IApplicationBuilder"/>.
+    /// </summary>
+    public static class ApplicationBuilderExtensions
+    {
+        /// <summary>
+        /// Enable <see cref="AspNetCoreHttpContext.Current"/> in the current request with adding <see cref="CurrentRequestContextMiddleware"/> to the application's request pipeline.
+        /// </summary>
+        /// <param name="builder">The application's request pipeline.</param>
+        /// <returns>The application's request pipeline added <see cref="CurrentRequestContextMiddleware"/>.</returns>
+        public static IApplicationBuilder UseCurrentRequestContext(this IApplicationBuilder builder)
+        {
+            if (builder == null)
+            {
+                throw new ArgumentNullException(nameof(builder));
+            }
+
+            return builder.UseMiddleware<CurrentRequestContextMiddleware>();
+        }
+    }
+}
